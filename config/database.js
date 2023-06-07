61f1c5e22fb812f4f4ca94e3efcf4b736f7407e0
sleep-lover <laleetsb2002@gmail.com>
2023-06-08 00:34:55 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,21 @@
+//here we configure the mongooes and connect to the data base;
+const mongoose=require('mongoose');
+const {MONGO_URL}=process.env;
+
+exports.connect = () => {
+    // Connecting to the database
+    mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      })
+      .then(() => {
+        console.log("Successfully connected to database");
+      })
+      .catch((error) => {
+        console.log("database connection failed. exiting now...");
+        console.error(error);
+        process.exit(1);
+      });
+  };
