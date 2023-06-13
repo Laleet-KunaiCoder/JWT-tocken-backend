9a414a17c11b7d79cc36dca2e69c66d19c5f1bbe
sleep-lover <laleetsb2002@gmail.com>
2023-06-14 00:42:08 +0530

    done

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 //here we configure the mongooes and connect to the data base;
-require('dotenv').config();
-const mongoose=require('mongoose');
-const MONGO_URI=process.env.MONGO_URI;
+require("dotenv").config();
+const mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGO_URI;
 exports.connect = () => {
-    // Connecting to the database
-    mongoose.connect(MONGO_URI, {
-      })
-      .then(() => {
-        console.log("Successfully connected to database");
-      })
-      .catch((error) => {
-        console.log("database connection failed. exiting now...");
-        console.error(error);
-        process.exit(1);
-      });
-  };
+  // Connecting to the database
+  mongoose
+    .connect(MONGO_URI, {})
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
+};
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
-const http = require('http');
-const app=require("./app");
-const server=http.createServer(app);
-const port=process.env.PORT;
-server.listen(port,()=>{
-    console.log(`listehing at ${port}`);
+require("dotenv").config();
+const http = require("http");
+const app = require("./app");
+const server = http.createServer(app);
+const port = process.env.PORT;
+server.listen(port, () => {
+  console.log(`listehing at ${port}`);
 });
-
